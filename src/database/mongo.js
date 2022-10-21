7b3435970e9f4f79c2e401fb74a8dacbbe000ca1
Luis Mendoza <luis23luis33@gmail.com>
2022-10-21 15:06:08 +0200

    despliegue

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -14,6 +14,7 @@ const dbConnect = () => {
         console.log("**** CONEXION CORRECTA ****");
       } else {
         console.log("***** ERROR DE CONEXION ****");
+        console.log("Error: ", err);
       }
     }
   );
