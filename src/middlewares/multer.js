6a3569306d8927487e8a435b75751dfebd30712f
Luis Mendoza <luis23luis33@gmail.com>
2022-10-21 22:50:35 +0200

    corrigiendo rutas estáticas

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 function uploadFiles() {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "/uploads");
+      cb(null, "public/uploads");
     },
     filename: (req, file, cb) => {
       const ext = file.originalname.split(".").pop();
